f81ee2463d56f4fbda563210040ef3bf190f7837
Gulzarokz <gokz22301@gamil.com>
2025-03-05 00:07:03 +0500

    This is our first javascript course chapter alert Ramadan pkage

---
+++ Ch_1_Alert/index.js
@@ -0,0 +1,27 @@
+//  chap:1 Alerts
+// what is alert
+// Alert is a pop up box which give user message Like
+// alert("Hi I'm Alert")
+// alert("Thanks for your input!");
+
+
+
+// alert is a keyword—
+// it can't be capitalized bcz js is case sensitive
+
+// Parenthesis is special requirement for js.
+// open parenthesis ( 
+// ) and close parenthesis
+// Quetation marks ""
+
+// Js ignores spaces but only after or before the string not in the middle of the strings
+// alert ( "Thanks for your input!" );
+
+// some developer may write window.alert 
+// window.alert("This is a global object")
+
+//:You can use single quotes intead of double quotes 
+ 
+
+alert('Your are singin successfully')
+

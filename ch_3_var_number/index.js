c7fc13d3c950d09a2ea23ea3fbcf582a558af20d
unknown <gokz22301@gmail.com>
2025-03-06 11:41:41 -0800

    Done js exercise chapter 3 var for number & rules for naming variables

---
+++ ch_3_var_number/index.js
@@ -0,0 +1,71 @@
+//variable for number
+
+// var myAge = 24;
+// console.log(myAge)
+
+// var myAge = "24";
+// console.log(typeof myAge)
+
+
+// var firstNumber = 2;
+// var secondNumber = "4"
+// var result = firstNumber +secondNumber;
+// console.log( typeof result)
+
+// var firstNumber = 2;
+// var secondNumber = 4
+// var result = firstNumber +secondNumber;
+// console.log(result)
+
+// var originalNum = 30;
+// var newNum = originalNum + 20;
+// console.log(newNum)
+
+//  var originalNum = 23;
+//  var numToBeAdded = 7;
+// var newNum = originalNum + numToBeAdded;
+// console.log(newNum)
+
+
+//  chapter no 4 Variable Names Legal and Illegal
+// You've already learned three rules about naming a variable:
+//  You can't enclose it inquotation marks.
+// const "var" = it illegal 
+//  The name can't be a number or start with a number.
+// var 123 = 23 it can't do like that bcz it illegal too
+//  It can't be any of JavaScript's keywords—the special words that act as programming instructions, like alert
+//  var function = 123;
+
+//  variable name can't contain any spaces.
+// var my name = "Gulzar" it is illegal
+
+// variable name can contain only letters, numbers, dollar signs, and underscores
+// var xyz = "alphbets "
+
+// var num1 = 13; it allowed at the end not at the start
+// var $sign = "America currency"
+
+// var _myName = "Hakim khan"
+// var my_Name = "Hakim khan"
+
+
+// Capital letters are fine, but be careful. Variable names are case sensitive. A rose is not a
+// Rose. If you assign the string "Floribundas" to the variable rose, and then ask JavaScript
+// for the value assigned to Rose, you'll come up empty.
+
+// var Rose = "name of flower";
+// console.log(Rose);
+
+// Camel case 
+
+// var myFristName = "Gulzar";
+// console.log(myFirstName)
+
+// userResponse
+// userResponseTime
+// userResponseTimeLimit
+
+
+// snakeCase
+// var my_First_Name = "Hakim";
+// console.log(my_First_Name)

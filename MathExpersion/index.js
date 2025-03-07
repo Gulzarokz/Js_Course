d626ba490ab04a0a71b7f8fe89e1b44882c88c9a
unknown <gokz22301@gmail.com>
2025-03-08 00:09:42 +0500

    Done chapter 5 math expersion with different examples

---
+++ MathExpersion/index.js
@@ -0,0 +1,79 @@
+// 5 Math expressions: Familiar operators
+// var popularNumber = 2 + 2;
+// alert(popularNumber);
+
+// The arithmetic operators I've been using, +, -, *, and /,% are undoubtedly familiar to you.
+// This one may not be:
+
+//  Addtion operators
+
+// var num =  3 + 4;
+//  3 and 4 are called operands but + operator is operator
+// console.log(num);
+
+
+// additions of two number
+// const num1 = 5;
+// const num2 = 3;
+// const results = num1 + num2;
+
+// concatenation
+
+// const results  =  2 + "6";
+
+// console.log(results);
+
+
+// const results = 2 + 6 + "2";
+// console.log(results);
+
+// const myName = "Gulzar";
+// console.log("Hello"  +myName + "how are you?");
+
+//String always converted to number when it is multiplied or divided
+
+// const results = 2 + 4 + "27" / 9;
+// console.log(results);
+
+// Minus operator -
+
+// const num1 = 5;
+// const num2 = 2;
+// const results = num1 - num2;
+// document.write( "The results of num1 and num2 =  " +results);
+
+// const results = 5 - 2 - "27" / 9;
+// console.log(results);
+
+// Multiplication operator *
+
+//  const num1 = 5;
+// const num2 = 2;
+// const results = num1 * num2;
+//  document.write( "The results of num1 and num2 =  " +results);
+
+//  const results = 5 * 2 * 27 / 3;
+// console.log(results);
+
+// division operator /
+// const num1 = 10;
+// const num2 = 2;
+// const results = num1 / num2;
+// console.log(results);
+
+// const results = 30 /2 / 5;
+// document.write( "The results of 36 / 27 / 3 =  " + results);
+
+// const results = 15 / "3";
+// console.log(results);
+
+
+// Modulo operator %
+
+//  const num1 = 10;
+//  const num2 = 3;
+//  const results = num1 % num2;
+//  console.log(results);
+
+// const results = 14 % 3;
+// console.log(results);
